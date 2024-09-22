0cd5ff9b14af75d229df8a090581d8391cfe7671
Fernando NUNEZ <fernunezmendoza@gmail.com>
2024-09-22 20:46:09 +0200

    ch5-10

--- main.go
+++ main.go
@@ -1,33 +1,57 @@
 package main
 
-func (e email) cost() int {
-	if e.isSubscribed {
-		return len(e.body) * 2
-	}
-	return len(e.body) * 5
-}
+func getExpenseReport(e expense) (string, float64) {
 
-func (e email) format() string {
-	var isSubscribedText string
-	if e.isSubscribed {
-		isSubscribedText = "Subscribed"
-	} else {
-		isSubscribedText = "Not Subscribed"
+	em, ok := e.(email)
+	if ok{
+		return em.toAddress, em.cost()
 	}
 
-	return "'"+e.body+"' | "+isSubscribedText
+	s, ok := e.(sms)
+	if ok{
+		return s.toPhoneNumber, s.cost()
+	}
+
+	return "", 0.0
+
+	// ?
 }
+
+// don't touch below this line
 
 type expense interface {
-	cost() int
-}
-
-type formatter interface {
-	format() string
+	cost() float64
 }
 
 type email struct {
 	isSubscribed bool
 	body         string
+	toAddress    string
+}
+
+type sms struct {
+	isSubscribed  bool
+	body          string
+	toPhoneNumber string
+}
+
+type invalid struct{}
+
+func (e email) cost() float64 {
+	if !e.isSubscribed {
+		return float64(len(e.body)) * .05
+	}
+	return float64(len(e.body)) * .01
+}
+
+func (s sms) cost() float64 {
+	if !s.isSubscribed {
+		return float64(len(s.body)) * .1
+	}
+	return float64(len(s.body)) * .03
+}
+
+func (i invalid) cost() float64 {
+	return 0.0
 }
 
