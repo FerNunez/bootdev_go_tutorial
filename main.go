488537093c898a116d05cf290f33cc5b74cd4520
Fernando NUNEZ <fernunezmendoza@gmail.com>
2024-09-24 22:13:54 +0200

    ch6-2

--- main.go
+++ main.go
@@ -4,28 +4,7 @@ import (
 	"fmt"
 )
 
-func sendSMSToCouple(msgToCustomer, msgToSpouse string) (int, error) {
-
-	c, err := sendSMS(msgToCustomer)
-	if err != nil {
-		return 0, err
-	}
-	s, err := sendSMS(msgToSpouse)
-	if err != nil {
-		return 0, err
-	}
-
-	return c + s, nil
-
+func getSMSErrorString(cost float64, recipient string) string {
+	return fmt.Sprintf("SMS that costs $%.2f to be sent to '%v' can not be sent", cost, recipient )
 }
 
-// don't edit below this line
-
-func sendSMS(message string) (int, error) {
-	const maxTextLen = 25
-	const costPerChar = 2
-	if len(message) > maxTextLen {
-		return 0, fmt.Errorf("can't send texts over %v characters", maxTextLen)
-	}
-	return costPerChar * len(message), nil
-}
