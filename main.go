a2c6182765114512a2cf349d85afefd3faa169f8
Fernando NUNEZ <fernunezmendoza@gmail.com>
2024-09-25 21:21:21 +0200

    ch7-2

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
-func bulkSend(numMessages int) float64 {
-	total :=0.0
+func maxMessages(thresh int) int {
 
-	for i:=1; i<=numMessages; i++{
-		total += 1.0 + (float64(i-1)* 0.01)
+
+	maxNumberMessages:=0
+	total := 0
+	for i:=0; total<=thresh; i++{
+		total+= 100+i
+		maxNumberMessages = i
 	}
-
-	return total
+	return maxNumberMessages
+	
 }
 
