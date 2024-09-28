97fee587ce34f07835aab7e267b59f708730382a
Fernando NUNEZ <fernunezmendoza@gmail.com>
2024-09-29 00:03:21 +0200

    ch8-10

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
-func getMessageCosts(messages []string) []float64 {
-	// ?
-	messagesCosts := make([]float64, len(messages))
+func sum(nums ...int) int {
 
-	for i:= 0; i<len(messages); i++{
-
-		messagesCosts[i] = float64(len(messages[i])) * 0.01
+	total := 0
+	for i:=0; i<len(nums); i++{
+		total += nums[i]
 		}
 
-	return messagesCosts[:]
+	return total
+
 }
 
