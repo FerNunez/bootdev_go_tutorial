3d209d94da1e7217b98b69cffdb4c7d39d2faff9
Fernando NUNEZ <fernunezmendoza@gmail.com>
2024-09-30 20:55:15 +0200

    ch8-11

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
-func sum(nums ...int) int {
+type cost struct {
+	day   int
+	value float64
+}
 
-	total := 0
-	for i:=0; i<len(nums); i++{
-		total += nums[i]
+func getCostsByDay(costs []cost) []float64 {
+
+	result := make([]float64, 1)
+
+	for i := 0; i < len(costs); i++ {
+		day := costs[i].day
+
+		for {
+			if day > len(result)-1 {
+				result = append(result, 0.)
+			} else {
+				break
+			}
 		}
 
-	return total
+		result[day] += costs[i].value
+	}
 
+	return result
 }
 
