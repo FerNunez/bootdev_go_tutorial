f91d8a021610232c684207c0540defc5b1bb7483
Fernando NUNEZ <fernunezmendoza@gmail.com>
2024-10-02 22:11:08 +0200

    ch9-1

--- main.go
+++ main.go
@@ -1,49 +1,23 @@
 package main
-
-import (
+import(
 	"errors"
-	"fmt"
 )
 
-// getLogger takes a function that formats two strings into
-// a single string and returns a function that formats two strings but prints
-// the result instead of returning it
-func getLogger(formatter func(string, string) string) func(string, string) {
-	return func (first string, second string){
-		fmt.Println(formatter(first, second))
+func getUserMap(names []string, phoneNumbers []int) (map[string]user, error) {
+
+	if len(names) != len(phoneNumbers){
+		return nil, errors.New("invalid sizes")
 	}
-}
+	result := make(map[string]user, len(names))
 
-// don't touch below this line
-
-func test(first string, errors []error, formatter func(string, string) string) {
-	defer fmt.Println("====================================")
-	logger := getLogger(formatter)
-	fmt.Println("Logs:")
-	for _, err := range errors {
-		logger(first, err.Error())
+	for i:=0; i<len(names); i++{
+		result[names[i]]= user{name: names[i], phoneNumber: phoneNumbers[i]}
 	}
+	return result, nil
 }
 
-func colonDelimit(first, second string) string {
-	return first + ": " + second
-}
-func commaDelimit(first, second string) string {
-	return first + ", " + second
+type user struct {
+	name        string
+	phoneNumber int
 }
 
-func main() {
-	dbErrors := []error{
-		errors.New("out of memory"),
-		errors.New("cpu is pegged"),
-		errors.New("networking issue"),
-		errors.New("invalid syntax"),
-	}
-	test("Error on database server", dbErrors, colonDelimit)
-
-	mailErrors := []error{
-		errors.New("email too large"),
-		errors.New("non alphanumeric symbols found"),
-	}
-	test("Error on mail server", mailErrors, commaDelimit)
-}
