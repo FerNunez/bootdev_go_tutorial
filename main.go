441968b00d55cdfc5a580631e5f271b22d618ed4
Fernando NUNEZ <fernunezmendoza@gmail.com>
2024-10-06 20:44:12 +0200

    ch9-10

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
-func getCounts(messagedUsers []string, validUsers map[string]int) {
+func getNameCounts(names []string) map[rune]map[string]int {
+	// Your code here
 
-for _, mu := range messagedUsers{
-	if _, ok := validUsers[mu]; ok {
+	result := make(map[rune]map[string]int)
+	for _, name := range names {
 
-			validUsers[mu]+=1
+		runes := []rune(name)
+
+		
+
+		if _, ok := result[runes[0]];!ok {
+			result[runes[0]] = make(map[string]int)
 
 		}
+
+		result[runes[0]][name] += 1
 	}
+	return result
 }
 
