af3678c84f148622625d899ee364f4f2284812c2
Fernando NUNEZ <fernunezmendoza@gmail.com>
2024-10-13 15:25:26 +0200

    ch9-c3

--- main.go
+++ main.go
@@ -1,53 +1,28 @@
 package main
 
-func findSuggestedFriends(username string, friendships map[string][]string) []string {
+const (
+	logDeleted  = "user deleted"
+	logNotFound = "user not found"
+	logAdmin    = "admin deleted"
+)
 
-	newSuggestions := make(map[string]bool)
-	alreadyFriend := make(map[string]bool)
-
-	// get username's friends
-	if userFriends, ok := friendships[username]; ok {
-
-		// add already friend into a map/hashset
-		for _, userFriendName := range userFriends {
-			alreadyFriend[userFriendName] = false
-		}
-
-		// loop friends friends
-		for _, userFriendName := range userFriends {
-
-			if friendsOfFriends, okThereIsMap := friendships[userFriendName]; okThereIsMap {
-
-				for _, friendsOfFriendName := range friendsOfFriends {
-
-					isNotUser := friendsOfFriendName != username
-					_, okAlreadyFriend := alreadyFriend[friendsOfFriendName]
-					_, okNewSuggestions := newSuggestions[friendsOfFriendName]
-
-					if isNotUser && !okAlreadyFriend && !okNewSuggestions {
-						newSuggestions[friendsOfFriendName] = true
-					}
-
-				}
-
-			}
-
-		}
-
-	}
-
-	if numbSugestions := len(newSuggestions); numbSugestions > 0 {
-
-		v := make([]string, 0, len(newSuggestions))
-
-		for key := range newSuggestions {
-			v = append(v, key)
-		}
-
-		return v
-
-	}
-
-	return nil
-
+type user struct {
+	name   string
+	number int
+	admin  bool
 }
+
+func logAndDelete(users map[string]user, name string) (log string) {
+
+	defer delete(users, name)
+
+	user, ok := users[name]
+	if !ok {
+		return logNotFound
+	}
+	if user.admin {
+		return logAdmin
+	}
+	return logDeleted
+}
+
