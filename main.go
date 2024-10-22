e2389cc17e27944f4b0af55d0b0b37c1364d4580
Fernando NUNEZ <fernunezmendoza@gmail.com>
2024-10-22 20:45:51 +0200

    ch10-6

--- main.go
+++ main.go
@@ -1,33 +1,18 @@
 package main
 
-type Analytics struct {
-	MessagesTotal     int
-	MessagesFailed    int
-	MessagesSucceeded int
-}
+import (
+	"strings"
+)
 
-type Message struct {
-	Recipient string
-	Success   bool
-}
+func removeProfanity(message *string) {
 
-// don't touch above this line
-
-// ?
-// When your function receives a pointer to a struct, you might try to access a field like this and encounter an error:
-//		msgTotal := *analytics.MessagesTotal
-// Instead, use this simple approach using a selector expression.
-//		msgTotal := analytics.MessagesTotal
-// This approach is shorthand for (*analytics).MessagesTotal and is the recommended, simplest way to access struct fields in Go.
-
-
-func getMessageText(a *Analytics, m Message) {
-	if m.Success {
-		a.MessagesSucceeded += 1
-	} else {
-		a.MessagesFailed += 1
+	if message == nil{
+		return
 	}
-
-	a.MessagesTotal += 1
+	messageVal := *message
+	messageVal = strings.ReplaceAll(messageVal, "fubb", "****")
+	messageVal = strings.ReplaceAll(messageVal, "shiz", "****")
+	messageVal = strings.ReplaceAll(messageVal, "witch", "*****")
+	*message = messageVal
 }
 
