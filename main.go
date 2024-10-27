1cc36c59a22c85f7055f7b00db495bc9bf14fedb
Fernando NUNEZ <fernunezmendoza@gmail.com>
2024-10-27 10:39:10 +0100

    ch12-5

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
-func addEmailsToQueue(emails []string) chan string {
+func countReports(numSentCh chan int) int {
+	count := 0
+	for {
+		if v, ok := <-numSentCh; ok {
+			count += v
+		} else {
+			break
+		}
 
-	channel := make(chan string, len(emails))
-
-	for _,e := range emails{
-		channel <- e
 	}
+	return count
+}
 
-	return channel
+// don't touch below this line
+
+func sendReports(numBatches int, ch chan int) {
+	for i := 0; i < numBatches; i++ {
+		numReports := i*23 + 32%17
+		ch <- numReports
+	}
+	close(ch)
 }
 
