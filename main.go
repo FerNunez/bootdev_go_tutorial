a293582dd187381e7bcfbbfd27ed8fd61ea240f3
Fernando NUNEZ <fernunezmendoza@gmail.com>
2024-10-31 14:39:06 +0100

    ch15-4

--- main.go
+++ main.go
@@ -1,14 +1,10 @@
 package main
+type emailStatus int
 
-import "fmt"
+const(
 
-func (a *analytics) handleEmailBounce(em email) error {
-	if ok := em.recipient.updateStatus(em.status); ok != nil {
-		return fmt.Errorf("error updating user status: %w", ok)
-	}
-	if ok := a.track(em.status); ok != nil {
-		return fmt.Errorf("error tracking user bounce: %w", ok)
-	}
-
-	return nil
-}
+	emailBounced emailStatus = iota
+	emailInvalid
+	emailDelivered
+	emailOpened
+)
